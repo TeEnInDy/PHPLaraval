75cda76b7097788bdf56fdb66bb2ea1fdf424e3c
TeEnInDy <98180053+TeEnInDy@users.noreply.github.com>
2023-12-26 00:20:07 +0700

    Finins

--- resources/views/myfolder/mypage.blade.php
+++ resources/views/myfolder/mypage.blade.php
@@ -1,10 +1,29 @@
- 
- <center>
-      <h1>สูตรคูณ หรือ สูตรใคร</h1>
-      <h1>ต้องการสูตรคูณแม่ไหนโปรดพิมพ์ใส่</h1>
-      <form method="post" action="/my-route">
-          @csrf
-          <input type="text" name="multiplier">
-          <button type="submit">ไปดูกันโล้ดด</button>
-      </form>
-  </center>
+<style>
+body {
+    background-image: url('https://media.giphy.com/media/zo7nbbrQKwdb2rSG4k/giphy.gif');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.content {
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 2px solid #333;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 50px auto;
+    max-width: 600px;
+}
+</style>
+<center>
+    <div class="content">
+        <h1>สูตรคูณ หรือ สูตรใคร</h1>
+        <h1>ต้องการสูตรคูณแม่ไหนโปรดพิมพ์ใส่</h1>
+        <form method="post" action="/my-route">
+            @csrf
+            <input type="text" name="multiplier">
+            <button type="submit">ไปดูกันโล้ดด</button>
+        </form>
+    </div>
+
+</center>
--- resources/views/myroute.blade.php
+++ resources/views/myroute.blade.php
@@ -6,24 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+body {
+    background-image: url('https://media.giphy.com/media/Qs0QEnugOy0xIsFkpD/giphy.gif');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.content {
+    width: 500px;
+    height: 600px;
+    background-color: rgba(255, 255, 255, 0.8);
+    text-align: center;
+    border: 5px dashed #0a1af3;
+    padding-top: 10px;
+    margin: 90px 25px;
+    transition: 1s;
+    
+}
+
+.content:hover {
+    background-color: rgba(81, 233, 182, 0.733);
+    box-shadow: 20px 20px #000000b7;
+}
+
+.content:hover {
+    transform: scale(1.3);
+}
+</style>
 <center>
 
     <body>
-        <div>
+        <div class="content">
             <?php
     $multiplier;
     $numberOfMultiples = 24;
     ?>
 
-            <h2>สูตรคูณแม่ <?php echo $multiplier; ?></h2>
-            <ul>
-                <?php
+            <h1>สูตรคูณแม่ <?php echo $multiplier; ?> </h1>
+            <?php
         for ($i = 1; $i <= $numberOfMultiples; $i++) {
             $result = $multiplier * $i;
             echo "$multiplier x $i = $result<br>";
         }
         ?>
-            </ul>
+
+
 
         </div>
 
